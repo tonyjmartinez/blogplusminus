eec15e7998781bdd77a7a98b6366d3f20f243e3e
tonyjmartinez <ajosephmartinez@gmail.com>
2019-02-28 21:08:16 -0800

    .

--- server/router.js
+++ server/router.js
@@ -1,5 +1,11 @@
+const Auth = require("./controllers/authentication");
+const passportService = require("./services/passport");
+const passport = require("passport");
+
 module.exports = function(app) {
   app.get("/", function(req, res) {
     res.send({ hello: "there" });
   });
+
+  app.post("/signup", Auth.signup);
 };
