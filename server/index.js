def99a111aac1b168b7459b1f7c8c12cfa73a044
tonyjmartinez <ajosephmartinez@gmail.com>
2019-02-28 19:59:38 -0800

    Initial commit. Working on server/auth

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const http = require("http");
+const router = require("./router");
+
+mongoose.connect("mongodb://localhost/auth", { useNewUrlParser: true });
+
+app.use(morgan("combined"));
+app.use(cors());
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on:", port);
