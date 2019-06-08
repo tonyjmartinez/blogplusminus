c628207500dd50d26a440dca32241c743efd8d5d
Tony Martinez <ajosephmartinez@gmail.com>
2019-06-07 19:29:50 -0700

    .

--- client/src/components/ui/Header.js
+++ client/src/components/ui/Header.js
@@ -1,73 +1,73 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import withAppContext from "../../context/withAppContext.js";
-import AuthDialog from "./AuthDialog";
-import { Link } from "react-router-dom";
-import Fab from "@material-ui/core/Fab";
-import AddIcon from "@material-ui/icons/Add";
-import MediaQuery from "react-responsive";
-import Avatar from "@material-ui/core/Avatar";
-import MoonIcon from "@material-ui/icons/Brightness3";
-import MoonIconOutlined from "@material-ui/icons/Brightness3Outlined";
+import React, {useState} from 'react';
+import {makeStyles} from '@material-ui/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import withAppContext from '../../context/withAppContext.js';
+import AuthDialog from './AuthDialog';
+import {Link} from 'react-router-dom';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import MediaQuery from 'react-responsive';
+import Avatar from '@material-ui/core/Avatar';
+import MoonIcon from '@material-ui/icons/Brightness3';
+import MoonIconOutlined from '@material-ui/icons/Brightness3Outlined';
 
 const useStyles = makeStyles({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   grow: {
-    flexGrow: 1
+    flexGrow: 1,
   },
   menuButton: {
     marginLeft: -12,
-    marginRight: 20
+    marginRight: 20,
   },
   link: {
-    textDecoration: "none",
-    color: "black"
+    textDecoration: 'none',
+    color: 'black',
   },
   topFab: {
-    marginRight: "20px"
+    marginRight: '20px',
   },
   fab: {
     margin: 0,
-    top: "auto",
+    top: 'auto',
     right: 20,
     bottom: 20,
-    left: "auto",
-    position: "fixed"
+    left: 'auto',
+    position: 'fixed',
   },
   extendedIcon: {
-    marginRight: "5px"
+    marginRight: '5px',
   },
   fabText: {
-    marginRight: "10px"
+    marginRight: '10px',
   },
   avatar: {},
   avatarDiv: {
-    display: "inline-block",
-    marginRight: "10px"
+    display: 'inline-block',
+    marginRight: '10px',
   },
   moonIcon: {
-    marginRight: "10px",
-    marginTop: "5px"
-  }
+    marginRight: '10px',
+    marginTop: '5px',
+  },
 });
 
-const header = props => {
+const header = (props) => {
   const auth = props.context.auth;
   const user = props.context.user;
   const setDarkMode = props.context.setDarkMode;
   const darkMode = props.context.darkMode;
 
   const [dialogOpen, setDialogOpen] = useState(false);
-  const [authType, setAuthType] = useState("login");
+  const [authType, setAuthType] = useState('login');
 
   const classes = useStyles();
-  const openDialog = type => {
+  const openDialog = (type) => {
     setAuthType(type);
     setDialogOpen(true);
   };
@@ -76,7 +76,7 @@ const header = props => {
     setDialogOpen(false);
   };
 
-  const handleSignOut = event => {
+  const handleSignOut = (event) => {
     props.context.signOut();
     closeDialog();
   };
@@ -104,7 +104,7 @@ const header = props => {
           </MediaQuery>
           <div className={classes.avatarDiv}>
             <Avatar
-              style={{ color: darkMode ? "white" : "black" }}
+              style={{color: darkMode ? 'white' : 'black'}}
               className={classes.avatar}
             >
               {user.username.charAt(0).toUpperCase()}
@@ -141,17 +141,17 @@ const header = props => {
           onClickClose={closeDialog}
           type={authType}
         />
-        <Button onClick={() => openDialog("login")} color="inherit">
+        <Button onClick={() => openDialog('login')} color="inherit">
           Log In
         </Button>
-        <Button onClick={() => openDialog("signup")} color="inherit">
+        <Button onClick={() => openDialog('signup')} color="inherit">
           Sign Up
         </Button>
       </div>
     );
   }
 
-  const Moon = () =>
+  const Moon = props =>
     darkMode ? (
       <MoonIcon
         className={classes.moonIcon}
@@ -170,10 +170,10 @@ const header = props => {
         <Toolbar>
           <Typography variant="h6" color="inherit" className={classes.grow}>
             <Link to="/" className={classes.link}>
-              Blog {"+/-"}
+              Blog {'+/-'}
             </Link>
           </Typography>
-          {Moon()}
+          <Moon/>
           {authButtons}
         </Toolbar>
       </AppBar>
--- client/src/components/ui/PostDialog.js
+++ client/src/components/ui/PostDialog.js
@@ -1,68 +1,68 @@
-import React, { useState } from "react";
-import { makeStyles } from "@material-ui/styles";
-import Dialog from "@material-ui/core/Dialog";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
-import CloseIcon from "@material-ui/icons/Close";
-import Slide from "@material-ui/core/Slide";
-import Button from "@material-ui/core/Button";
-import TextField from "@material-ui/core/TextField";
-import Fab from "@material-ui/core/Fab";
-import MediaQuery from "react-responsive";
-import AddIcon from "@material-ui/icons/Add";
-import withAppContext from "../../context/withAppContext";
-import PostForm from "./post/PostForm";
+import React from 'react';
+import {makeStyles} from '@material-ui/styles';
+import Dialog from '@material-ui/core/Dialog';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Fab from '@material-ui/core/Fab';
+import MediaQuery from 'react-responsive';
+import AddIcon from '@material-ui/icons/Add';
+import withAppContext from '../../context/withAppContext';
+import PostForm from './post/PostForm';
 const useStyles = makeStyles({
   appBar: {
-    position: "relative"
+    position: 'relative',
   },
   flex: {
-    flex: 1
+    flex: 1,
   },
   submitButton: {
-    background: "none",
-    border: "none",
-    color: "inherit"
+    background: 'none',
+    border: 'none',
+    color: 'inherit',
   },
   container: {
-    display: "flex",
-    flexWrap: "wrap"
+    display: 'flex',
+    flexWrap: 'wrap',
   },
 
   textFieldDiv: {
-    margin: "0px auto",
-    width: "100%"
+    margin: '0px auto',
+    width: '100%',
   },
   form: {
-    margin: "0px auto",
-    display: "flex",
-    flexWrap: "wrap"
+    margin: '0px auto',
+    display: 'flex',
+    flexWrap: 'wrap',
   },
 
   fabDiv: {
-    margin: "0px auto",
-    marginTop: "20px"
+    margin: '0px auto',
+    marginTop: '20px',
   },
   fabText: {
-    marginRight: "10px"
-  }
+    marginRight: '10px',
+  },
 });
 
-const Transition = props => {
+const Transition = (props) => {
   return <Slide direction="up" {...props} />;
 };
 
-const postDialog = props => {
+const postDialog = (props) => {
   const classes = useStyles();
 
   const handleNewPost = async (title, content) => {
     const newPost = await props.context.newPost(
-      props.context.user.id,
-      title,
-      content,
-      props.context.user.username
+        props.context.user.id,
+        title,
+        content,
+        props.context.user.username
     );
     props.onClose();
     console.log(newPost);
@@ -70,12 +70,12 @@ const postDialog = props => {
 
   const getWidth = () => (
     <MediaQuery minWidth={700}>
-      {matches => {
+      {(matches) => {
         let width;
         if (matches) {
-          width = "60%";
+          width = '60%';
         } else {
-          width = "80%";
+          width = '80%';
         }
         return width;
       }}
@@ -84,7 +84,7 @@ const postDialog = props => {
 
   return (
     <MediaQuery minWidth={700}>
-      {matches => {
+      {(matches) => {
         let desktop = false;
         if (matches) {
           desktop = true;
@@ -96,7 +96,7 @@ const postDialog = props => {
             desktop={desktop}
             {...props}
           />
-        ))
+        ));
         return (
           <Dialog
             fullScreen
