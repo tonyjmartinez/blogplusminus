f8db89873a3d09f84d781840a541cc0782403448
tonyjmartinez <ajosephmartinez@gmail.com>
2019-06-10 23:34:50 -0700

    .

--- client/src/components/post/post.js
+++ client/src/components/post/post.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import { graphql } from 'react-apollo';
-import query from '../../queries/post';
-import PostCard from '../frontPage/PostCard';
-import Comment from './comment';
-import withAppContext from '../../context/withAppContext';
-
+import React from "react";
+import { graphql } from "react-apollo";
+import query from "../../queries/post";
+import PostCard from "../frontPage/PostCard";
+import Comment from "./comment";
+import withAppContext from "../../context/withAppContext";
 
 const post = props => {
   console.log(props);
   const ctxProps = props.context;
   const username = !props.data.loading ? props.data.post.username : null;
-  console.log(ctxProps);
-  console.log(username);
+  const comments = props.data.comments;
+
   const { darkMode } = ctxProps;
 
   if (props.data.loading) {
     return <div>Loading...</div>;
   }
 
+  const Comments = props => {
+    console.log(comments);
+    if (comments !== undefined && comments.length > 0) {
+      return (
+        <Comment comments={comments} darkMode={darkMode} username={username} />
+      );
+    }
+    return null;
+  };
+
   return (
     <React.Fragment>
       <PostCard post={props.data.post} />
-      <Comment darkMode={darkMode} username={username} />
+      <Comments />
     </React.Fragment>
-
-
   );
 };
 
 export default graphql(query, {
-  options: props => ({ variables: { postId: props.match.params.id } }),
+  options: props => ({ variables: { postId: props.match.params.id } })
 })(withAppContext(post));
--- client/src/components/ui/UserAvatar.js
+++ client/src/components/ui/UserAvatar.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import Avatar from '@material-ui/core/Avatar';
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
 
 const userAvatar = props => {
-    const { darkMode, username } = props;
-    return (
-        <Avatar
-            style={{ color: darkMode ? 'white' : 'black' }}
-        >
-            {username !== undefined ? username.charAt(0).toUpperCase() : null}
-        </Avatar>
-    );
+  const { darkMode, username } = props;
+  return (
+    <Avatar style={{ color: darkMode ? "white" : "black" }}>
+      {username ? username.charAt(0).toUpperCase() : null}
+    </Avatar>
+  );
 };
 
 export default userAvatar;
