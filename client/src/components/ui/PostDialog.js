28098afe09c77a3ad1bb3b7b54455123f6d9a874
Tony Martinez <ajosephmartinez@gmail.com>
2019-07-19 22:40:47 -0700

    Switched react-responsive to use hooks.

--- client/src/components/ui/PostDialog.js
+++ client/src/components/ui/PostDialog.js
@@ -1,52 +1,45 @@
 import React from 'react';
 import Dialog from '@material-ui/core/Dialog';
 import Slide from '@material-ui/core/Slide';
-import MediaQuery from 'react-responsive';
 import withAppContext from '../../context/withAppContext';
 import PostForm from './post/PostForm';
+import { useMediaQuery } from 'react-responsive';
 
 const Transition = React.forwardRef(function Transition(props, ref) {
-  return <Slide direction="up" ref={ref} {...props} />;
+  return <Slide direction='up' ref={ref} {...props} />;
 });
 
 const postDialog = props => {
   console.log(props);
   const handleNewPost = async (title, content) => {
     const newPost = await props.context.newPost(
-        props.context.user.id,
-        title,
-        content,
-        props.context.user.username,
-        props.context.user.token
+      props.context.user.id,
+      title,
+      content,
+      props.context.user.username,
+      props.context.user.token
     );
     props.onClose();
     console.log(newPost);
   };
 
+  const desktop = useMediaQuery({ query: '(min-width: 850px)' });
+  console.log('desktop', desktop);
+
   return (
-    <MediaQuery minWidth={700}>
-      {(matches) => {
-        let desktop = false;
-        if (matches) {
-          desktop = true;
-        }
-        return (
-          <Dialog
-            fullScreen
-            open={props.open}
-            onClose={props.onClose}
-            TransitionComponent={Transition}
-          >
-            <PostForm
-              onClose={props.onClose}
-              newPost={handleNewPost}
-              desktop={desktop}
-              {...props}
-            />
-          </Dialog>
-        );
-      }}
-    </MediaQuery>
+    <Dialog
+      fullScreen
+      open={props.open}
+      onClose={props.onClose}
+      TransitionComponent={Transition}
+    >
+      <PostForm
+        onClose={props.onClose}
+        newPost={handleNewPost}
+        desktop={desktop}
+        {...props}
+      />
+    </Dialog>
   );
 };
 
